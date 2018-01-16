6014f62b9444ef0f84d5cbd66d7acf1127cfbe2e
Daron Jones <daron.jones@grow.am>
2018-01-16 18:40:10 +0000

    updating api

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-/*global exports, console*/
-exports.handler = function (event, context) {
-	'use strict';
-	console.log(event);
-	context.succeed('hello world');
-};
+/* global require, module */
+var ApiBuilder = require('claudia-api-builder');
+var api = new ApiBuilder();
+
+api.get("/hello/{name}/{superpower}", function (request) {
+    var name = request.pathParams.name;
+    return 'Hello World - meet ' + name + 'has superpower' + request.pathParams.superpower;
+});
+
+module.exports = api;
